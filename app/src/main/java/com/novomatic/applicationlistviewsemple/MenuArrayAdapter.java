564931bd09ba86595f7ca19030ac9d8d1c42c27b
unknown <fpirazzi@W7-IT13-PF.gmatica.lan>
2015-03-03 16:04:04 +0100

    Initial commit

---
+++ app/src/main/java/com/novomatic/applicationlistviewsemple/MenuArrayAdapter.java
@@ -0,0 +1,42 @@
+package com.novomatic.applicationlistviewsemple;
+
+/**
+ * Created by fpirazzi on 27/02/2015.
+ */
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MenuArrayAdapter extends ArrayAdapter<NovoMenuItem> {
+    private final Context context;
+    private final NovoMenuItem[] values;
+
+    public MenuArrayAdapter(Context context, NovoMenuItem[] values) {
+        super(context, R.layout.novo_menu_item, values);
+        this.context = context;
+        this.values = values;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        LayoutInflater inflater = (LayoutInflater) context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        View rowView = inflater.inflate(R.layout.novo_menu_item, parent, false);
+
+        TextView firstLine = (TextView) rowView.findViewById(R.id.firstLine);
+        TextView secondLine = (TextView) rowView.findViewById(R.id.secondLine);
+        ImageView imageView = (ImageView) rowView.findViewById(R.id.icon);
+
+        firstLine.setText(values[position].Nome);
+        secondLine.setText(values[position].Icona);
+
+        return rowView;
+    }
+}
---
+++ app/src/main/java/com/novomatic/applicationlistviewsemple/NovoMenuItem.java
@@ -0,0 +1,22 @@
+package com.novomatic.applicationlistviewsemple;
+
+import java.security.PublicKey;
+
+/**
+ * Created by fpirazzi on 27/02/2015.
+ */
+public class NovoMenuItem {
+
+    public NovoMenuItem(){
+
+    }
+
+    public NovoMenuItem(String nome, String icona ){
+        this.Nome = nome;
+        this.Icona = icona;
+    }
+
+    public String Nome;
+    public String Icona;
+
+}
